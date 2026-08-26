25c7b60cc3bd8dceac0dcedb19a1350c7d860f29
Database Project <iamdushanl@github.com>
2026-08-26 08:32:58 +0530

    added helmet

--- server/src/app.js
+++ server/src/app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const helmet = require('helmet');
 
 const ApiResponse = require('./utils/ApiResponse');
 const taskRoutes = require('./routes/taskRoutes');
@@ -9,6 +10,7 @@ const notFound = require('./middleware/notFound');
 
 const app = express();
 
+app.use(helmet());
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
